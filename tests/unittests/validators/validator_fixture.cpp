ebb4968278133a5fbdf583edf0acbc3b486e2d1a
Jaeyoung <swe.jaeyoungpark@navercorp.com>
2018-10-08 02:27:25 +0900

    Add validator_fixture.cpp

---
+++ tests/unittests/validators/validator_fixture.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Jaeyoung on 2018. 10. 7..
+//
+
+#include <gtest/gtest.h>
+#include <validators/validator.h>
+
+class ValidatorFixture : public ::testing::Test {
+protected:
+    virtual void TearDown() {
+
+    }
+
+    virtual void SetUp() {
+
+    }
+
+public:
+    ValidatorFixture() : Test() {
+        validator = new Validator(std::string("[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*")
+                                  + "@"
+                                  + "[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}");
+    }
+
+    virtual ~ValidatorFixture() {
+        delete validator;
+    }
+
+    Validator* validator;
+};
+
+TEST_F(ValidatorFixture, check) {
+    EXPECT_TRUE(validator->isValid("abc@gmail.com"));
+}
